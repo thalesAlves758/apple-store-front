b9d4559f266c13ac1e7942ece80fca880b8eb821
Thales Alves <thalesalves758@gmail.com>
2022-07-07 22:59:40 -0300

    feat: implement sign-up and sign-in links

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -2,6 +2,7 @@ import Container from "../layout/Container";
 import Main from "../layout/Main";
 import Logo from "../layout/Logo";
 import SignInForm from "./SignInForm";
+import StyledLink from "../layout/StyledLink";
 
 export default function SignIn() {
   return (
@@ -9,6 +10,9 @@ export default function SignIn() {
       <Main>
         <Logo />
         <SignInForm />
+        <StyledLink to="/sign-up">
+          Ainda não possui uma conta? Cadastre-se agora!
+        </StyledLink>
       </Main>
     </Container>
   );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,6 +2,7 @@ import Container from "../layout/Container";
 import Main from "../layout/Main";
 import Logo from "../layout/Logo";
 import SignUpForm from "./SignUpForm";
+import StyledLink from "../layout/StyledLink";
 
 export default function SignIn() {
   return (
@@ -9,6 +10,7 @@ export default function SignIn() {
       <Main>
         <Logo />
         <SignUpForm />
+        <StyledLink to="/sign-in">Já possui uma conta? Entre agora!</StyledLink>
       </Main>
     </Container>
   );
